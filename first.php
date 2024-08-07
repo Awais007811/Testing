835ffd4ad50f2e8859261f7b9ab0776614861440
Awais007811 <awaisali7395@gmail.com>
2024-08-07 00:03:49 -0700

    First Commit Locally

---
+++ first.php
@@ -0,0 +1,18 @@
+<?php
+
+	echo "<h1 style='color:red; text-align:center; margin-top:20%'>Hello PHP</h1>";
+	?>
+	<div>
+		
+	<form action="#" method="post" style="">
+		<input type="text" name="f_name" >
+		<input type="text" name="l_name" >
+		<input type="submit" name="signup" value="SignUP">
+		
+	</form>
+	</div>
+	<?php
+
+
+?>
+
